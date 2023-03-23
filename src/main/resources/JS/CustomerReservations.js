e7ca87b67ea11404d992e6cfbc6cbaa7a57ff8f7
BasmaJawad <basm0035@stud.kea.dk>
2023-03-23 16:28:28 +0100

    css

--- src/main/resources/JS/CustomerReservations.js
+++ src/main/resources/JS/CustomerReservations.js
@@ -4,6 +4,7 @@ let customerReservations = [];
 
 async function fetchCustomerReservation(){
     if (loggedCustomer != null) {
+        showCustomerName(loggedCustomer);
         let urlGetCustomerReservations = "http://localhost:8080/customerReservations/";
         urlGetCustomerReservations += loggedCustomer["email"];
         customerReservations = await fetchAny(urlGetCustomerReservations);
@@ -23,6 +24,14 @@ async function fetchCustomerReservation(){
 
 }
 
+function showCustomerName(customer) {
+    const customerName = document.getElementById("customerName");
+    customerName.innerHTML =
+        "<div> Navn:   " + customer.firstname + " " + customer.lastname + "</div>" + "<br>" +
+        "<div> Email:   " + customer.email+ "</div>" +
+        "<div> Tlf. Nummer:   " + customer.number + "</div>";
+}
+
 function fetchAny(url) {
     console.log(url)
     return fetch(url).then((response) => response.json())
@@ -37,12 +46,24 @@ function displayReservations(reservation){
     const reservationInfo = document.createElement("p");
     reservationInfo.innerHTML =
         "<img id='cardPic' src=\"../static/images/Billede2.png\">"+
-        "<span>" + reservation["activity"].name  + "</span>" + "<br>" +
+        "<span class='resName'>" + reservation["activity"].name  + "</span>" + "<br>" +
         reservation.date + "<br>" +
         "Deltagere: " + reservation.participants + "<br>" +
         "Pris: " + reservation.price + " kr." + "<br>" +
-        "Tid: " + reservation["activity"].startTime + "<br>" +
-        reservation.status;
+        "Tid: " + reservation["activity"].startTime + "<br> <br>" +
+        "<span class='status'>" + reservation.status +"</span>"
+
+    const statusElement = reservationInfo.querySelector('.status');
+
+    if (reservation.status === "PENDING") {
+        statusElement.classList.add("pending");
+    } else if (reservation.status === "CONFIRMED") {
+        statusElement.classList.add("confirmed");
+    } else if (reservation.status === "CANCELLED") {
+        statusElement.classList.add("cancelled");
+    }
+
+
 
     //reservationCard.appendChild(picture);
         reservationCard.appendChild(reservationInfo);
