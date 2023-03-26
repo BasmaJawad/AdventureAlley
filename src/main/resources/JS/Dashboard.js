43e93bd22d3ebad3185f81bd472eeaf745547eee
BasmaJawad <basm0035@stud.kea.dk>
2023-03-27 00:38:05 +0200

    dashboard done

--- src/main/resources/JS/Dashboard.js
+++ src/main/resources/JS/Dashboard.js
@@ -1,20 +1,24 @@
 let reservationsThisMonth = [];
 let reservationsToday = [];
 let pendingReservations = [];
-document.addEventListener('DOMContentLoaded', setup);
 
-
+window.addEventListener("load", setup);
 function fetchAny(url) {
     console.log(url)
     return fetch(url).then((response) => response.json())
 }
-function setup() {
-    fetchReservationsByMonth();
-    fetchReservationsByDay();
-    fetchPendingReservations();
-
+async function setup() {
+    //promise.all is used to wait for all the promises to be resolved before continuing
+    await Promise.all([
+        fetchReservationsByMonth(),
+        fetchReservationsByDay(),
+        fetchPendingReservations(),
+    ])
+    displayKPIs()
+    displayRevenueThisMonth()
+    displayPopularActivity()
 }
-window.addEventListener("load", displayKPIs);
+
 async function fetchReservationsByMonth() {
     const currentDate = new Date();
     const currentMonth = currentDate.getMonth() + 1;
@@ -60,4 +64,62 @@ function displayKPIs(){
     totalPendingReservations.appendChild(tprP)
 
 }
+function displayRevenueThisMonth(){
+    const revenueThisMonth = document.getElementById("revenue");
+    revenueThisMonth.textContent = calculateTotalReservationsThisMonth() + " dkk"
 
+    const thisMonth = document.getElementById("month");
+    thisMonth.textContent = new Date().toLocaleString('default', { month: 'long'});
+
+}
+
+function calculateTotalReservationsThisMonth(){
+    let totalReservationsThisMonth = 0;
+
+    reservationsThisMonth.forEach(reservation => {
+    if (reservation.status != "CANCELLED")
+        totalReservationsThisMonth += reservation.price
+    })
+    return totalReservationsThisMonth;
+}
+
+
+function displayPopularActivity(){
+    const popularActivity = document.getElementById("popActivityImg");
+    const popularActivityName = findPopularActivity();
+
+    if (popularActivityName == "Paintball"){
+        popularActivity.src = "../static/images/img_7.png"
+    }
+
+}
+function findPopularActivity(){
+
+    //lasertag og gokart eksisterer ikke i databasen endnu
+        const activityCount = {
+            Paintball: 0,
+            Lasertag: 0,
+            GoKart: 0,
+        }
+
+        //looper gennem alle reservationer
+        //hver gang den finder en reservation med samme aktivitet navn, tæller den op
+        // [] bruges til at acessere et objekts properties
+        reservationsThisMonth.forEach((reservation) => {
+            activityCount[reservation.activity.name]++;
+        });
+
+        let mostPopActivity;
+
+        let maxCount = 0;
+
+        //for in loop
+        for (const activity in activityCount) {
+            if (activityCount[activity] > maxCount) { //hvis current activity er større end maxCount
+                mostPopActivity = activity; //sæt mostPopActivity til at være current activity
+                maxCount = activityCount[activity]; //sætter maxCount til at være current activity
+            }
+        }
+
+        return mostPopActivity;
+}
